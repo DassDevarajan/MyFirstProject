95c09b57fe53bf8cb3e2225cd633a646a8f35106
Dass Devarajan <dass.devarajan@gmail.com>
2019-07-09 11:04:15 +0530

    Added display name to the class

--- calculator/src/test/java/com/company/app/CalculatorTest.java
+++ calculator/src/test/java/com/company/app/CalculatorTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Basic Calculator")
 class CalculatorTest {
 
 	private Calculator calc;

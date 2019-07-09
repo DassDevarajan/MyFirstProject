8c191a158455521e69be142bcefa3ea63abe9676
Dass Devarajan <dass.devarajan@gmail.com>
2019-07-09 10:50:56 +0530

    Initial checkin

---
+++ calculator/src/test/java/com/company/app/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.company.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	private Calculator calc;
+	
+	@BeforeEach
+	public void init() {
+		calc = new Calculator();
+	}
+	
+	@Test
+	public void addition() {
+		assertEquals(30, calc.add(20, 10));
+	}
+	
+	@Test
+	public void subtraction() {
+		assertEquals(20, calc.subtract(50, 30));
+	}
+	
+	@Test
+	public void multiplication() {
+		assertEquals(50, calc.multiply(25, 2));
+	}
+	
+	@Test
+	public void division() {
+		assertEquals(20, calc.divide(100, 5));
+	}
+}
